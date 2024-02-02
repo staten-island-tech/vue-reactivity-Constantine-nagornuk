ce7de4945792627ba3781adcc2a9b98c519eb2a5
Constantine-nagornuk <124071530+Constantine-nagornuk@users.noreply.github.com>
2024-02-02 13:13:38 -0500

    w

---
+++ vite/src/components/TheWelcome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+
+    <h1 v-if="answer">Correct</h1>
+    <h1 v-else>Incorrect</h1>
+  </div>
+</template>
+
+<script setup>
+  const answer = true
+  
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
